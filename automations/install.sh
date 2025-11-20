f31b8dac89836bebfcb9b2208228d764085af9b5
vgeruso <victor.geruso@gmail.com>
2025-11-19 23:19:11 -0300

    first commit

---
+++ automations/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "[Error]:: Name of script is required"
+    echo "[Use]:: > $0 <name-of-script>"
+    exit 1
+fi
+
+PATH_SCRIPT="$HOME/Documents/projects/scripts/$1"
+PATH_BIN="/usr/bin/$1"
+
+if [ ! -f "$PATH_SCRIPT" ]; then
+    echo "[Error]:: Script not found"
+    exit 1
+fi
+
+if [ -f "$PATH_BIN" ]; then
+    sudo rm "$PATH_BIN"
+fi
+
+if [ "$(stat -c %a "$PATH_SCRIPT")" != "777" ]; then
+    sudo chmod 777 "$PATH_SCRIPT"
+fi
+
+if [ "$(stat -c %a "$PATH_SCRIPT")" != "777" ]; then
+    echo "[Error]:: Failed to change permissions"
+    exit 1
+fi
+
+sudo cp "$PATH_SCRIPT" "$PATH_BIN"
---
+++ setup/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install git
+# Create folder projects on Documents
+# Clone my-scripts on Documents/projects
+# Install docker
+# Install docker Compose
+# Install postgresql with docker
+# Install tmux
+# Install htop
+# Install zsh
+# Install oh-my-zsh
+# recovery .zshrc
+# Install apidog
+# Install dbeaver
+# Install obsidian
+# Install discord
+# Install zed
+# Install cursor
+# Create folder ok-snack on Documents/projects
+# Install scripts autmations
+# Install steam
